a91d64e6357e9fcf9c40b9aa2de74d518f6fb242
Gordo1705 <102055188+Gordo1705@users.noreply.github.com>
2024-06-04 19:53:30 -0300

    Add files via upload

--- Codigo/App.js
+++ Codigo/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('/home/administrador/my-app/assets/escudo.jpg')}
+          style={width:100px;}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text>franco</Text>
+        <Text>blas</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "grey",
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', // Alinear el contenido en el centro vertical
+  },
+  textContainer: {
+    marginBottom: 50,
+  },
+  imageContainer: {
+    marginBottom: 20, // Espacio entre la imagen y el texto
+  },
+  image: {
+    width: 200, // Ajustar el ancho de la imagen según tus necesidades
+    height: 200, // Ajustar la altura de la imagen según tus necesidades
+    resizeMode: 'contain', // Ajustar la imagen dentro del tamaño especificado
   },
 });
